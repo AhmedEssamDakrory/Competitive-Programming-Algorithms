b0633d0709f7dc674ca3779e754d81110e3eb1fd
AhmedEssamDakrory <ae00498@gmail.com>
2020-01-09 01:00:16 +0200

    comments

--- strings/KMP.cpp
+++ strings/KMP.cpp
@@ -1,8 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef pair<int, int>ii;
+typedef long long ll;
 #define sz(s) int((s).size())
+#define mkp make_pair
+#define debug(x) cout<<(#x)<<": \""<<x<<"\""<<endl;
+#define sc(x) scanf("%d", &(x))
+#define all(x) (x).begin(), (x).end()
+#define F first
+#define S second
 const int N = (int) 2e5+1;
+const int inf = (int) 1e9;
+const int alpha_size = 26;
 
+//there is a minor edit, lps stores length-1 so that I can start string from 0...
 int lps[N]; //this array to store the length of longest proper suffix which is also a proper prefix for string.....
 string p , t; // p is the pattern you wish to find in string t....
 vector<int> pos; // this vector to store the positions where the pattern is matched (start position)...
@@ -12,7 +23,7 @@ void Build_LPS(){
 
 	int j = -1;
 	lps[0] = -1;
-	for(int i = 1 ; i< sz(p) ; ++i){
+	for(int i = 1 ; i < sz(p) ; ++i){
 		while(j > -1 && p[j+1] != p[i])
 			j = lps[j];
 		if(p[j+1] == p[i]) ++j;
@@ -31,7 +42,7 @@ bool Match(){
 			if(j != -1) j = lps[j];
 			else ++i;
 		}
-		if(j == sz(p)-1) j = lps[j],pos .push_back(i-sz(p));
+		if(j == sz(p)-1) j = lps[j],pos.push_back(i-sz(p));
 	}
 
 	return sz(pos) > 0;
@@ -50,4 +61,5 @@ int main()
 	else puts("NO");
 
 }
+
 
